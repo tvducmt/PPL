8568b5a435ce1fb699691b763b7d43520d826604
Duc Tran <tvducmt@gmail.com>
2018-09-07 19:38:49 +0700

    Add

--- src/main/mp/parser/MP.g4
+++ src/main/mp/parser/MP.g4
@@ -11,23 +11,106 @@ options{
 
 program  : (many_declare)+ EOF;
 many_declare : variable_declare | function_declare | procedure_declare;
-variable_declare : VAR varlist ;
+variable_declare : VAR varlist ;    
 varlist : variable SEMI (varlist)*;
 variable : idlist COLON vartype;
 idlist : ID (COMMA idlist)*;
 vartype : primitive_type | compound_type ;
 primitive_type: INTTYPE | STRINGTYPE | BOOLEANTYPE | REALTYPE ;
 compound_type: ARRAY LSB  INTLIT DD INTLIT  RSB OF primitive_type;
-function_declare : FUNCTION ID LB parameterlist RB COLON vartype SEMI variable_declare compound_statement;
+function_declare : FUNCTION ID LB parameterlist RB COLON vartype SEMI variable_declare compound_state;
 parameterlist : variable  parameterl | ;
 parameterl : SEMI variable parameterl | ;
-compound_statement : BEGIN .*? END;//nullable_statement END;
-//nullable_statement : (statement)*;
-// statement:   assignment_state | if_state   | for_state | while_state | break_state | continue_state
-//             | return_state | call_state | compound_state | with_function;
-// assignment_state : (ID ASSIGN (ID)*)+ expression;
+procedure_declare : PROCEDURE ID LB parameterlist RB SEMI variable_declare? compound_state;
+compound_state : BEGIN nullable_stmt END;
+nullable_stmt : (stmt)*;
+stmt:   exprStmt
+            | assign_state | if_state | for_state
+            | while_state | break_state | continue_state
+            | return_state | call_state | compound_state | with_state|default_function;
 
-procedure_declare : PROCEDURE ID LB parameterlist RB SEMI variable_declare compound_statement;
+assign_state : (ID ASSIGN (ID)*)+ expr SEMI;
+if_state : IF expr THEN stmt (ELSE stmt)?;
+for_state : FOR ID ASSIGN expr (TO|DOWNTO) expr DO stmt;
+while_state : WHILE expr DO stmt SEMI;
+break_state : BREAK SEMI;
+continue_state : CONTINUE SEMI;
+return_state : RETURN expr SEMI;
+with_state : WITH  varlist DO stmt;
+call_state : fullcall SEMI;
+
+fullcall: ID LB exprList RB ;
+
+//Built-in function
+default_function: GETINT LB RB SEMI 
+                | PUTINT LB expr RB SEMI
+                | PUTINTLN LB expr RB SEMI
+                | GETFLOAT LB RB SEMI
+                | PUTFLOAT LB expr RB SEMI
+                | PUTFLOATLN LB expr RB SEMI
+                | PUTBOOL LB expr RB SEMI
+                | PUTBOOLLN LB expr RB SEMI
+                | PUTSTRING LB expr RB SEMI
+                | PUTSTRINGLN LB expr RB SEMI
+                | PUTLN LB RB SEMI; 
+
+GETINT: 'getInt';
+
+PUTINT: 'putInt';
+
+PUTINTLN: 'putIntLn';
+
+GETFLOAT: 'getFloat';
+
+PUTFLOAT: 'putFloat';
+
+PUTFLOATLN: 'putFloatLn';
+
+PUTBOOL: 'putBool';
+
+PUTBOOLLN: 'putBoolLn';
+
+PUTSTRING:  'putString';
+
+PUTSTRINGLN: 'putStringLn';
+
+PUTLN: 'putLn';
+
+//expression
+exprStmt: expr SEMI;
+exprList: (expr (COMMA expr)*)?;
+expr: expr AND THEN expr1
+    | expr OR ELSE expr1
+    | expr1;
+
+expr1: expr2 EQUAL expr2 
+    | expr2 NOTEQUAL expr2
+    | expr2 LESSTHAN expr2
+    | expr2 LESOREQUAL expr2
+    | expr2 GREATERTHAN expr2
+    | expr2 GREOREQUAL expr2 
+    | expr2;
+
+expr2: expr2 ADDITION expr3 
+    |  expr2 SUBORNE expr3 
+    |  expr2 OR expr3 
+    | expr3;
+
+expr3: expr3 DIVISION expr4
+     | expr3 MULTIPLICATION expr4
+     | expr3 MOD expr4
+     | expr3 AND expr4
+     | expr3 INTDIV expr4
+     | expr4;
+
+expr4:  NOT expr4
+     |  SUBORNE expr4
+     |  expr5;
+
+expr5: LB expr RB
+     | INTLIT | FLOATLIT | STRINGLIT | BOOLEANLIT | ID | fullcall;
+
+
 
 //Lexer----------------------------------------------
 
@@ -124,15 +207,15 @@ GREATERTHAN: '>';
 
 GREOREQUAL: '>=';
 
-NOT: 'not';
+NOT: ('n'|'N')('o'|'O')('t'|'T');
 
-AND: 'and'; 
+AND: ('a'|'A')('N'|'n')('D'|'d');
 
-OR : 'or';
+OR : ('o'|'O')('r'|'R');
 
-INTDIV : 'div';
+INTDIV : ('d'|'D')('I'|'i')('V'|'v');
 
-MOD : 'mod';
+MOD : ('m'|'M')('O'|'o')('D'|'d');
 
 
 //Keyword
@@ -144,7 +227,7 @@ keywords: 	BREAK | CONTINUE | FOR | TO | DOWNTO | DO | IF | THEN | ELSE | RETURN
 BREAK: ('b'|'B')('r'|'R')('e'|'E')('a'|'A')('k'|'K');
 CONTINUE: ('c'|'C')('o'|'O')('N'|'n')('T'|'t')('i'|'I')('n'|'N')('u'|'U')('E'|'e'); 
 FOR :  ('f'|'F')('O'|'o')('r'|'R');
-TO : ('t|T')('o'|'O');
+TO : ('t'|'T')('o'|'O');
 DOWNTO: ('d'|'D')('o'|'O')('w'|'W')('n'|'N')('T'|'t')('o'|'O');
 DO: ('D'|'d')('o'|'O');
 IF :  ('i'|'I')('f'|'F');
@@ -161,7 +244,7 @@ ARRAY: ('a'|'A')('r'|'R')('r'|'R')('a'|'A')('y'|'Y');
 OF:  ('o'|'O')('f'|'F');
 TRUE : ('t'|'T')('r'|'R')('U'|'u')('E'|'e');
 FALSE: ('f'|'F')('a'|'A')('l'|'L')('s'|'S')('E'|'e');
-
+WITH: ('W'|'w')('i'|'I')('T'|'t')('H'|'h');
 
 
 //comment
@@ -181,15 +264,17 @@ ID: ([a-zA-Z]|'_')([a-zA-Z0-9]|'_')*;
 //Error
 fragment ILLEGAL: ('\\'~('b'|'f'|'r'|'n'|'t'|'"'|'\\'|'\''));
 
-ILLEGAL_ESCAPE: '"' (~[\n"\\]|ESCAPE)* ILLEGAL {
-                                            String s = getText();
-                                            s = s.substring(1,s.length());
-                                            setText(s);
-                                        };
-UNCLOSE_STRING: '"'(~[\n"\\]|ESCAPE)*  {
-                                            String s = getText();
-                                            s = s.substring(1,s.length());
-                                            setText(s);
-                                        };
+ILLEGAL_ESCAPE: '"' (~[\n"\\]|ESCAPE)* ILLEGAL ;
+                            // {
+                            //                 String s = getText();
+                            //                 s = s.substring(1,s.length());
+                            //                 setText(s);
+                            //             };
+UNCLOSE_STRING: '"'(~[\n"\\]|ESCAPE)*  ;
+// {
+//                                             String s = getText();
+//                                             s = s.substring(1,s.length());
+//                                             setText(s);
+//                                         };
 
 ERROR_CHAR: .;
--- src/test/ParserSuite.py
+++ src/test/ParserSuite.py
@@ -2,6 +2,7 @@ import unittest
 from TestUtils import TestParser
 
 class ParserSuite(unittest.TestCase):
+     #test var declar
     def test_variable_declar(self):
         """test variable declar"""
         input="""var a , b , c : integer ;"""
@@ -22,6 +23,7 @@ class ParserSuite(unittest.TestCase):
         input="""d , b : array [    1 .. 5    ] of integer ;"""
         expect = "Error on line 1 col 0: d"
         self.assertTrue(TestParser.test(input,expect,204))
+    
     #test function declar
     def test_function_declar(self):
         """test function declar """
@@ -32,8 +34,8 @@ class ParserSuite(unittest.TestCase):
             end"""
         expect = "successful"
         self.assertTrue(TestParser.test(input,expect,205))
-    def test_function_declar_error(self):
-        """test function declar """
+    def test_function_declar_error_para(self):
+        """test function declar para """
         input="""function foo (a , b : integer  ;) : array [ 1 .. 2 ] of integer ;
             var x , y : real ; 
             begin
@@ -41,7 +43,7 @@ class ParserSuite(unittest.TestCase):
             end"""
         expect = "Error on line 1 col 32: )"
         self.assertTrue(TestParser.test(input,expect,206))
-    def test_function_declar_para_empty(self):
+    def test_function_empty_para(self):
         """test function declar """
         input="""function foo () : array [ 1 .. 2 ] of integer ;
             var x , y : real ; 
@@ -50,14 +52,307 @@ class ParserSuite(unittest.TestCase):
             end"""
         expect = "successful"
         self.assertTrue(TestParser.test(input,expect,207))
+    def test_wrong_miss_close(self):
+        """test function declar """
+        input="""function foo ( : array [ 1 .. 2 ] of integer ;
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "Error on line 1 col 15: :"
+        self.assertTrue(TestParser.test(input,expect,208))
+    def test_function_named_error(self):
+        """test function declar """
+        input="""function 1foo (): array [ 1 .. 2 ] of integer ;
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "Error on line 1 col 9: 1"
+        self.assertTrue(TestParser.test(input,expect,209))  
+    def test_function_named(self):
+        """test function named """
+        input="""function _ADVCDfoo     (): array [ 1 .. 2 ] of integer ;
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "successful"
+        self.assertTrue(TestParser.test(input,expect,210))  
+    def test_wrong_miss_colon(self):
+        """test wrong miss colon """
+        input="""function foo ()  array [ 1 .. 2 ] of integer ;
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "Error on line 1 col 17: array"
+        self.assertTrue(TestParser.test(input,expect,211))
+    def test_wrong_miss_body(self):
+        """test wrong miss body """
+        input="""function foo () : array [ 1 .. 2 ] of integer ;
+            var x , y : real ;  """
+           
+        expect = "Error on line 2 col 32: <EOF>"
+        self.assertTrue(TestParser.test(input,expect,212))
+    def test_wrong_miss_return(self):
+        """ test wrong miss return """
+        input="""function foo ();
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "Error on line 1 col 15: ;"
+        self.assertTrue(TestParser.test(input,expect,213))
+    def test_wrong_miss_semi(self):
+        """test wrong miss semi """
+        input="""function foo () : array [ 1 .. 2 ] of integer 
+            var x , y : real ;
+            begin
+            
+            end"""
+        expect = "Error on line 2 col 12: var"
+        self.assertTrue(TestParser.test(input,expect,214))
+    def test_wrong_miss_semi_var(self):
+        """test wrong miss semi var """
+        input="""function foo () : array [ 1 .. 2 ] of integer ;
+            var x , y : real 
+            begin
+            
+            end"""
+        expect = "Error on line 3 col 12: begin"
+        self.assertTrue(TestParser.test(input,expect,215))
 
-
-
-
-
-
-
-
+    def test_wrong_miss_close_body(self):
+        """test wrong miss body end"""
+        input="""function foo () : array [ 1 .. 2 ] of integer ;
+            var x , y : real ; 
+            begin"""
+        expect = "Error on line 3 col 17: <EOF>"
+        self.assertTrue(TestParser.test(input,expect,216))
+    def test_case_insensitive(self):
+        """test_case_insensitive"""
+        input="""FuNctIon _ADVCDfoo     (): array [ 1 .. 2 ] of integer ;
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "successful"
+        self.assertTrue(TestParser.test(input,expect,217))
+    def test_wrong_type_return(self):
+        """test_wrong_type_return"""
+        input="""function _ADVCDfoo     (): array [ 1 .. 2 ] of array [ 1 .. 2 ] ;
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "Error on line 1 col 47: array"
+        self.assertTrue(TestParser.test(input,expect,218))
+    #test procedure declar
+    def test_procedure_declar(self):
+        """test_procedure_declar"""
+        input="""procedure abc ();
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "successful"
+        self.assertTrue(TestParser.test(input,expect,219))
+    def test_procedure_declar(self):
+        """test_procedure_declar"""
+        input="""procedure abc ();
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "successful"
+        self.assertTrue(TestParser.test(input,expect,219))
+    def test_case_insensitive(self):
+        """test_case_insensitive"""
+        input="""prOceDure _ADVCDfoo ();
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "successful"
+        self.assertTrue(TestParser.test(input,expect,220))
+    def test_wrong_redundancy_colon(self):
+        """test_wrong_redundancy_colon"""
+        input="""procedure abc (): array [ 1 .. 2 ] of integer ;
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "Error on line 1 col 16: :"
+        self.assertTrue(TestParser.test(input,expect,221))
+    def test_wrong_miss_semi_produre(self):
+        """test_wrong_redundancy_colon"""
+        input="""procedure abc ()
+            var x , y : real ; 
+            begin
+            
+            end"""
+        expect = "Error on line 2 col 12: var"
+        self.assertTrue(TestParser.test(input,expect,222))
+    def test_wrong_miss_body_produre(self):
+        """test_wrong_miss_body_produre """
+        input="""procedure foo ();
+            var x , y : real ;  """
+           
+        expect = "Error on line 2 col 32: <EOF>"
+        self.assertTrue(TestParser.test(input,expect,223))
+    #test Assignment Statement
+    def test_assignment_stmt_simple(self):
+        """test_assignment_stmt"""
+        input= """procedure abc ();
+            var x , y : real ; 
+            begin
+                a:=12;
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,224))
+    def test_assignment_stmt_many(self):
+        """test_assignment_stmt_many"""
+        input= """procedure abc ();
+            begin
+                a:=b:=c:=d:=12;
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,225))
+    def test_assign_stmt_with_expr(self):
+        """test_assign_stmt_with_expr"""
+        input= """procedure abc ();
+            begin
+                a:=b:=c:=d:=(12+3);
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,226))
+    def test_assign_stmt_with_funcall(self):
+        """test_assign_stmt_with_expr"""
+        input= """procedure abc ();
+            begin
+                a:=b:=c:=d:=foo();
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,227))
+    def test_assign_stmt_with_funcall_semi(self):
+        """test_assign_stmt_with_expr"""
+        input= """procedure abc ();
+            begin
+                a:=b:=c:=d:=foo(2+3;);
+            end"""
+        expect='Error on line 3 col 35: ;'
+        self.assertTrue(TestParser.test(input,expect,228))
+    def test_assign_stmt_with_funcall_explist(self):
+        """test_assign_stmt_with_funcall_explist"""
+        input= """procedure abc ();
+            begin
+                a:=b:=c:=d:=foo(2+3, 3*3, a AND b);
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,229))
+    def test_assign_stmt_with_realtype(self):
+        """test_assign_stmt_with_funcall_explist"""
+        input= """procedure abc ();
+            begin
+                some_real := 37573.5 * 37593 + 385.8 / 367.1;
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,230))
+        
+    #test_if_state
+    def test_ifstate(self):
+        """test_ifstate"""
+        input= """procedure abc ();
+            begin
+                if color = red then
+                   a:=3;
+                else
+                    b:=3;
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,231))
+    def test_ifstate_miss_else(self):
+        """test_ifstate_miss_else"""
+        input= """procedure abc ();
+            begin
+                if color = red then
+                    b:=3;
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,232))
+    def test_ifstate_miss_bodyif(self):
+        """test_ifstate_miss_bodyif"""
+        input= """procedure abc ();
+            begin
+                if color = red then
+            end"""
+        expect='Error on line 4 col 12: end'
+        self.assertTrue(TestParser.test(input,expect,233))
+    def test_ifstate_miss_exp(self):
+        """test_ifstate_miss_exp"""
+        input= """procedure abc ();
+            begin
+                if  then
+                    b:=3;
+            end"""
+        expect='Error on line 3 col 20: then'
+        self.assertTrue(TestParser.test(input,expect,234))
+    def test_ifstate_wrong_exp(self):
+        """test_ifstate_wrong_exp"""
+        input= """procedure abc ();
+            begin
+                if +hello then
+                    b:=3;
+            end"""
+        expect='Error on line 3 col 19: +'
+        self.assertTrue(TestParser.test(input,expect,235))
+    def test_ifstate_wrong_exp_real(self):
+        """test_ifstate_wrong_exp_real"""
+        input= """procedure abc ();
+            begin
+                if 3//4 then
+                    b:=3;
+            end"""
+        expect='Error on line 4 col 20: b'
+        self.assertTrue(TestParser.test(input,expect,236))
+    def test_ifstate_have_semi(self):
+        """test_ifstate_have_semi"""
+        input= """procedure abc ();
+            begin
+                if color = red then;
+                    hk:=56;
+            end"""
+        expect='Error on line 3 col 35: ;'
+        self.assertTrue(TestParser.test(input,expect,237))
+    def test_ifstate_have_semi_else(self):
+        """test_ifstate_have_semi_else"""
+        input= """procedure abc ();
+            begin
+                if color = red then
+                   a:=3;
+                else;
+                    b:=3;
+            end"""
+        expect='Error on line 5 col 20: ;'
+        self.assertTrue(TestParser.test(input,expect,238))
+    #test_for_state 
+    def test_for_state_to(self):
+        """test_for_state_to"""
+        input= """procedure ABC ();
+            begin
+                for i:= 1 to 10 do g:=5;
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,239))
+    def test_for_state_downto(self):
+        """test_for_state_downto"""
+        input= """procedure ABC ();
+            begin
+                for i:= 1 downto 10 do g:=5;
+            end"""
+        expect='successful'
+        self.assertTrue(TestParser.test(input,expect,240))
 
 
 
